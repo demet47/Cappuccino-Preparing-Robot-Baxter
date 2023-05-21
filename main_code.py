e4e68edec8db1193b98aa651bc27485ef27ac67a
salimtirit <salimtirit58@gmail.com>
2023-05-21 13:19:29 +0300

    fix voice recognition

--- main_code.py
+++ main_code.py
@@ -1,122 +1,127 @@
 import speech_recognition as sr
 from contextlib import suppress
-import pyttsx3
 import coffee_make as coffee
-from pydub import AudioSegment
-from pydub.playback import play
+from gtts import gTTS
+import pygame
+import time
+
 
 state_counter = 0
-
-
-def text_to_speech(text, output_file):
-    # Initialize the pyttsx3 engine
-    engine = pyttsx3.init('espeak-ng')
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
-
-    # Save the speech output to a file
-    engine.save_to_file(text, output_file)
-
-    # Run the engine to process the text
-    engine.runAndWait()
-
-
+bool_name = False
+#initiate coffee maker
 coffee_maker = coffee.Coffee()
 
 
-#with suppress(Exception):
+def __text_to_speech__(text, output_file):
+    # Initialize the pyttsx3 engine
+    tts = gTTS(
+        text=text,
+        lang="en"
+    )
+    tts.save(output_file)
 
-# set up the recognizer
-r = sr.Recognizer()
 
-# set up the microphone
-mic = sr.Microphone()
 
-# set the keyword we want to detect
-    
-# adjust the microphone sensitivity
-with mic as source:
-    r.adjust_for_ambient_noise(source)
+def __display_sound__(response_recording):
+    pygame.mixer.music.load(response_recording)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(1)
 
-# start listening
-print("Say something!")
-while True:
-    print("HELLOOOO 1")
 
+with suppress(Exception):
+
+    # set up the recognizer
+    r = sr.Recognizer()
+
+    # set up the microphone
+    mic = sr.Microphone()
+
+    #init pygame for mp3 playback
+    pygame.mixer.init()
+
+    # adjust the microphone sensitivity
     with mic as source:
-        audio = r.listen(source)
-    
-    print("HELLOOOO 2")
+        r.adjust_for_ambient_noise(source)
 
-    # use Google speech recognition to transcribe the audio
-    try:
-        print("HELLOOOO 3")
 
-        text = r.recognize_google(audio)
-        print("HELLOOOO 4")
+    # start listening
+    print("Say something!")
+    while True:
 
-        print("You said: " + text)
-        
-        # check if the keyword is in the recognized text
-        lower_case = text.lower()
-        if((state_counter == 0) & (lower_case.__contains__("hello"))):
-            baxter_response = "Hi there, so nice to have you here! Could you give me the honour of acquiring your name?"
-            response_recording = "baxter_greeting.wav"
-            text_to_speech(baxter_response, response_recording)
-            audio = AudioSegment.from_file(response_recording, format='wav')
-            play(audio)
-            state_counter = state_counter + 1
-            print("In state: ", state_counter - 1)
+        with mic as source:
+            audio = r.listen(source)
 
-        elif(state_counter == 1):
-            baxter_response = f"Welcome to the best cappuccino stand in the world {text}. I need to learn your sugar preference before I get started. Would you like low sugar coffee or high sugar coffee?"
-            response_recording = "baxter_sugar_request.wav"
-            text_to_speech(baxter_response, response_recording)
-            audio = AudioSegment.from_file(response_recording, format='wav')
-            play(audio)
-            state_counter = state_counter + 1
-            print("In state: ", state_counter - 1)
-
-        elif((state_counter == 2)):
-            if(text.__contains__("low sugar")):
-                baxter_response = "What a healthy choice congratulations! It will be ready in a minute."
-                response_recording = "baxter_preparation_initalization_low_milk.wav"
-                text_to_speech(baxter_response, response_recording)
-                audio = AudioSegment.from_file(response_recording, format='wav')
-                play(audio)
-                state_counter = state_counter + 1
-                print("In state: ", state_counter - 1)
-                coffee_maker.prepare(low_sugar = True)
-                #TODO: Low sugar preparation
-            elif(text.__contains__("high sugar")):
-                baxter_response = "Please beware, sugar is not that healthy a choice. It will be ready in a minute."
-                response_recording = "baxter_preparation_initalization_high_milk.wav"
-                text_to_speech(baxter_response, response_recording)
-                audio = AudioSegment.from_file(response_recording, format='wav')
-                play(audio)
-                state_counter = state_counter + 1
-                coffee_maker.prepare(low_sugar = False)
-                print("In state: ", state_counter - 1)
+        # use Google speech recognition to transcribe the audio
+        try:
+            if bool_name:
+                text = r.recognize_google(audio, language="tr-TR") 
             else:
-                baxter_response = "I couldn't undestand. Could you please say merely high sugar or low sugar?"
-                response_recording = "baxter_clarification_sugar.wav"
-                text_to_speech(baxter_response, response_recording)
-                audio = AudioSegment.from_file(response_recording, format='wav')
-                play(audio)
-                print("In state: ", state_counter)
-        elif(state_counter == 3):
-            baxter_response = f"Here is your coffee dear {text}. It was a pleasure serving you."
-            response_recording = "baxter_sugar_farewell.wav"
-            text_to_speech(baxter_response, response_recording)
-            audio = AudioSegment.from_file(response_recording, format='wav')
-            play(audio)
-            print("In state: ", state_counter - 1)
-            state_counter = 0
+                text = r.recognize_google(audio)
 
-    except sr.UnknownValueError:
-        print("Sorry, I didn't understand that.")
-    except sr.RequestError:
-        print("Sorry, something went wrong with the API request.")
+            print("You said: " + text)
+            
+            # check if the keyword is in the recognized text
+            lower_case = text.lower()
+            if((state_counter == 0) & (lower_case.__contains__("hello"))):
+                baxter_response = "Hi there, so nice to have you here! Could you give me the honour of acquiring your name?"
+                response_recording = "baxter_greeting.mp3"
+                __text_to_speech__(baxter_response, response_recording)
+                __display_sound__(response_recording)
+                state_counter = state_counter + 1
+                print("In state: ", state_counter - 1)
+                bool_name = True
 
+            elif(state_counter == 1):
+                baxter_response = f"Welcome to the best cappuccino stand in the world {text}. I need to learn your sugar preference before I get started. Would you like low sugar coffee or high sugar coffee?"
+                response_recording = "baxter_sugar_request.mp3"
+                __text_to_speech__(baxter_response, response_recording)
+                __display_sound__(response_recording)
+                state_counter = state_counter + 1
+                print("In state: ", state_counter - 1)
+                bool_name = False
+
+            elif((state_counter == 2)):
+                if(text.__contains__("low sugar")):
+                    baxter_response = "What a healthy choice congratulations! It will be ready in a minute."
+                    response_recording = "baxter_preparation_initalization_low_milk.mp3"
+                    __text_to_speech__(baxter_response, response_recording)
+                    __display_sound__(response_recording)
+                    state_counter = state_counter + 1
+                    print("In state: ", state_counter - 1)
+                    coffee_maker.prepare(low_sugar = True)
+                    #TODO: Low sugar preparation
+                elif(text.__contains__("high sugar")):
+                    baxter_response = "Please beware, sugar is not that healthy a choice. It will be ready in a minute."
+                    response_recording = "baxter_preparation_initalization_high_milk.mp3"
+                    __text_to_speech__(baxter_response, response_recording)
+                    __display_sound__(response_recording)
+                    state_counter = state_counter + 1
+                    coffee_maker.prepare(low_sugar = False)
+                    print("In state: ", state_counter - 1)
+                else:
+                    baxter_response = "I couldn't undestand. Could you please say merely high sugar or low sugar?"
+                    response_recording = "baxter_clarification_sugar.mp3"
+                    __text_to_speech__(baxter_response, response_recording)
+                    __display_sound__(response_recording)
+                    print("In state: ", state_counter)
+            elif(state_counter == 3):
+                baxter_response = f"Here is your coffee dear {text}. It was a pleasure serving you."
+                response_recording = "baxter_sugar_farewell.mp3"
+                __text_to_speech__(baxter_response, response_recording)
+                __display_sound__(response_recording)
+                print("In state: ", state_counter - 1)
+                state_counter = 0
+
+        except sr.UnknownValueError:
+            baxter_response = f"Sorry, I didn't understand that."
+            response_recording = "non-comprehending_baxter.mp3"
+            __text_to_speech__(baxter_response, response_recording)
+            __display_sound__(response_recording)
+        except sr.RequestError:
+            baxter_response = f"Sorry, something went wrong with the API request."
+            response_recording = "non-comprehending_baxter.mp3"
+            __text_to_speech__(baxter_response, response_recording)
+            __display_sound__(response_recording)
 
 
